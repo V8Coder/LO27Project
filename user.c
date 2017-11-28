b3a1e7c1fd54bd7de98350fe5066318ab9764979
v8coder <v8coder@protonmail.ch>
2017-11-28 22:30:12 +0100

    Ajout de user

--- user.c
+++ user.c
@@ -1,3 +1,33 @@
+/**Name of the file : user.c
+*Goal of the file : containing all the user methods and atributes
+*Author : Lucas Celant
+*E-mail : lucas.celant@utbm.fr
+*Last modification : 28/11/2017
+*/
 
+#include <string.h>
+#include <stdlib.h>
 
 #include "user.h"
+
+User createUser(long Name)
+{
+	User u = NULL;
+	u = (User) malloc(sizeof(User_struct));
+	return u;
+}
+
+long getID(User u)
+{
+	return u->ID;
+}
+
+char* getName(User u)
+{
+	return u->Name;
+}
+
+void setName(User u, char name[])
+{
+	strcpy(u->Name,name);
+}
