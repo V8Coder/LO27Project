bf3a65e99badf0e0cb02f16a0600d648bb902bbd
test <29061014+V8Coder@users.noreply.github.com>
2017-11-28 17:50:40 +0100

    debut

---
+++ registry.h
@@ -0,0 +1,13 @@
+
+/**Name of the file : registry.h
+*Goal of the file : Make a library which support secure transaction
+*Author : Lucas CELANT
+*E-mail : lucas.celant@utbm.fr
+*Last modification : 28/11/2017
+*/
+
+#ifndef _REGISTRY_H_
+#define _REGISTRY_H_
+
+
+#endif
