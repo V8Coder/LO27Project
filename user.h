261c26ed291b3cc03a952847c906ab208e3c214e
test <29061014+V8Coder@users.noreply.github.com>
2017-11-28 17:54:24 +0100

    Update user.h

--- user.h
+++ user.h
@@ -1,1 +1,6 @@
-
+/**Name of the file : user.h
+*Goal of the file : containing all the user methods and atributes
+*Author : Lucas Celant
+*E-mail : lucas.celant@utbm.fr
+*Last modification : 28/11/2017
+*/
