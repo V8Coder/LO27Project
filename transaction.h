b2840ac51eb6fa8ec0225892bb6fa754d2725bc1
test <celant@live.fr>
2017-11-28 23:28:24 +0100

    implementation transaction type

--- transaction.h
+++ transaction.h
@@ -8,11 +8,26 @@
 #ifndef _TRANSACTION_H_
 #define _TRANSACTION_H_
 
+
+typedef enum Status
+{
+    PENDING, CONFIRMED, EXECUTED, CANCELED
+} Status;
+
 /*
  * the abstract datatype transaction
  */
 typedef struct Transaction_struct {
 	long ID;
-	char Name[MaxNameLenght];
+	double Amount;
+	long EmitterID;
+	long RecipentID;
+	Status TransactionStatus;
+	long StartDateTime;
+	long EndDateTime;
+	long TTL;
+	char PrivateKey[];
 } Transaction_struct;
 typedef Transaction_struct* Transaction;
+
+#endif
