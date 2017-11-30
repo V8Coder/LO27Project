bd2444d5651ca1831a26e4fdf7fd4da1e9464d3f
test <celant@live.fr>
2017-11-30 17:38:37 +0100

    beginning of implementation of transaction.c

--- transaction.c
+++ transaction.c
@@ -7,4 +7,44 @@
 
 #include "transaction.h"
 
+Transaction createTransaction()
+{
+
+}
+
+double getAmount(Transaction t)
+{
+	return(t->Amount);
+}
+
+TransactionStatus getStatus(Transaction t)
+{
+	return(t->Status);
+}
+
+long getStartDateTime(Transation t)
+{
+	return(t->StartDateTime);
+}
+
+long getEndDateTime(Transation t)
+{
+	return(t->EndDateTime);
+}
+
+long getTTL(Transaction t)
+{
+	return(t->TTL);
+}
+
+bool isExpired(Transaction t, long d)
+{
+	if(t->EndDateTime >= d)
+		return true;
+	else
+		return false;
+}
+
 int main(){return 0;}
+
+
